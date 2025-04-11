ebc75895c8f54d2d412ce4308b6493c1c8798cae
dharmikvalnai57 <dharmikvalnai57@gmail.com>
2025-04-11 13:23:21 +0530

    Env config added

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
+import os
+from dotenv import load_dotenv
 
+load_dotenv() 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///marketplace.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'safkfjjasfwe7q3847394'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS', 'False').lower() in ('true', '1', 'yes')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 class Base(DeclarativeBase):
     pass
